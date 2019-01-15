114c91e9eb25964deaabb023455875df881dfe80
Jonathan Lattanzi <jonathanlattanzi@gmail.com>
2019-01-15 14:41:07 -0600

    Added CRUD

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-import './App.css';
+import './App.css'
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from './Header/Header'
+import Compose from './Compose/Compose'
+import Post from './Post/Post'
+
+const baseURL = 'https://practiceapi.devmountain.com/api'
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super()
 
-    this.state = {
-      posts: []
-    };
+        this.state = {
+            posts: [],
+        }
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
-  }
-  
-  componentDidMount() {
+        this.updatePost = this.updatePost.bind(this)
+        this.deletePost = this.deletePost.bind(this)
+        this.createPost = this.createPost.bind(this)
+    }
 
-  }
+    componentDidMount() {
+        // fetch posts
+        axios
+            .get(`${baseURL}/posts`)
+            .then((res) => {
+                this.setState({ posts: res.data })
+            })
+            .catch((err) => console.log(`error: ${err}`))
+    }
 
-  updatePost() {
-  
-  }
+    updatePost(id, text) {
+        axios
+            .put(`${baseURL}/posts?id=${id}`, { text })
+            .then((res) => this.setState({ posts: res.data }))
+            .catch((err) => console.log(`error: ${err}`))
+    }
 
-  deletePost() {
+    deletePost(id) {
+        axios
+            .delete(`${baseURL}/posts?id=${id}`)
+            .then((res) => this.setState({ posts: res.data }))
+            .catch((err) => console.log(`error: ${err}`))
+    }
 
-  }
+    createPost(text) {
+        axios
+            .post(`${baseURL}/posts`, { text })
+            .then((res) => this.setState({ posts: res.data }))
+            .catch((err) => console.log(`error: ${err}`))
+    }
 
-  createPost() {
+    render() {
+        const { posts } = this.state
 
-  }
+        return (
+            <div className='App__parent'>
+                <Header />
 
-  render() {
-    const { posts } = this.state;
-
-    return (
-      <div className="App__parent">
-        <Header />
-
-        <section className="App__content">
-
-          <Compose />
-          
-        </section>
-      </div>
-    );
-  }
+                <section className='App__content'>
+                    <Compose createPostFn={this.createPost} />
+                    {posts.map((post) => (
+                        <Post
+                            key={post.id}
+                            id={post.id}
+                            text={post.text}
+                            date={post.date}
+                            updatePostFn={this.updatePost}
+                            deletePostFn={this.deletePost}
+                        />
+                    ))}
+                </section>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
-import ProfileIcon from 'react-icons/lib/md/person-outline';
+import React, { Component } from 'react'
 
-import './Compose.css';
+import ProfileIcon from 'react-icons/lib/md/person-outline'
+
+import './Compose.css'
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
 
 export default class Compose extends Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      text: ''
-    };
+    constructor() {
+        super()
 
-    this.createPost = this.createPost.bind( this );
-  }
+        this.state = {
+            text: '',
+        }
 
-  updateText( text ) {
-    this.setState({ text });
-  }
+        this.createPost = this.createPost.bind(this)
+    }
 
-  createPost() {
+    updateText(text) {
+        this.setState({ text })
+    }
 
-  }
+    createPost() {
+        this.props.createPostFn(this.state.text)
+        this.setState({ text: '' })
+    }
 
-  render() {
-    // Destructuring
-    const { text } = this.state;
+    render() {
+        // Destructuring
+        const { text } = this.state
 
-    return (
-      <section className="Compose__parent">
-        <div className="Compose__top">
+        return (
+            <section className='Compose__parent'>
+                <div className='Compose__top'>
+                    <div className='Compose__profile-picture'>
+                        <ProfileIcon />
+                    </div>
 
-          <div className="Compose__profile-picture">
-            <ProfileIcon />
-          </div>
+                    {/* This is where you type the message for your new post */}
+                    <input
+                        className='Compose__input'
+                        placeholder="What's on your mind?"
+                        value={text}
+                        onChange={(e) => this.updateText(e.target.value)}
+                    />
+                </div>
 
-          {/* This is where you type the message for your new post */}
-          <input className="Compose__input"
-                 placeholder="What's on your mind?"
-                 value={ text }
-                 onChange={ ( e ) => this.updateText( e.target.value ) } />
-
-        </div>
-
-        <div className="Compose__bottom">
-          <button onClick={ this.createPost }>Compose</button>
-        </div>
-      </section>
-    )
-  }
+                <div className='Compose__bottom'>
+                    <button onClick={this.createPost}>Compose</button>
+                </div>
+            </section>
+        )
+    }
 }
